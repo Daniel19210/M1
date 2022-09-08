bc618e3902601ab58142225e2bf5bd25fefc4df9
Daniel Pinson <dp065247@MI105-15.iem>
2022-09-08 15:13:35 +0200

    TP1

---
+++ sysD/TP1/Exo2/compteFactory.java
@@ -0,0 +1,4 @@
+public class compteFactory(){
+    Map<int, Compte> listCompte;
+
+}
---
+++ sysD/TP1/Exo2/operation.java
@@ -0,0 +1,9 @@
+public class Operation {
+    string operation;
+    float montant;
+
+    public Operation(string op, float mt){
+        this.operation = op;
+        this.montant = mt;
+    }
+}
