3381049d43aa1bd00dd13945dae037ba0d561c12
Daniel Pinson <daniel.pinson19@gmail.com>
2022-10-24 17:56:09 +0200

    Fin tp de réseau

--- Reseaux/TP2/client.java
+++ Reseaux/TP2/client.java
@@ -1,22 +1,47 @@
 /*CLIENT CLASS*/
-import java.net.*;
 import java.io.*;
-public class client
-{
-            public static void main(String args[])throws Exception
-            {         
-                        byte b[]=new byte[Integer.parseInt(args[1])];
-                        FileInputStream f=new FileInputStream("test.txt");
-                        DatagramSocket dsoc=new DatagramSocket(2000);
-                        int i=0;
-                        while(f.available()!=0)
-                        {
-                                    b[i]=(byte)f.read();
-                                    i++;
-                        }                     
-                        f.close();
-                        dsoc.send(new DatagramPacket(b,i,InetAddress.getLocalHost(),Integer.parseInt(args[0])));
-            }
+import java.net.*;
 
+public class client {
+  
+  // Permet d'envoyer un paquet et d'attendre un paquet en retour
+  static void sendPaquet(DatagramSocket soc, byte[] pB, int tailleP, int portS, int portC){
+    byte reponse[] = new byte[100]; // L'acquittement est de taille fixe
+    try {
+      soc.send( new DatagramPacket( pB, tailleP, InetAddress.getLocalHost(), portS));
+      soc.receive(new DatagramPacket(reponse, 100, InetAddress.getLocalHost(), portC));
+      System.out.print("reponse = " + new String(reponse));
+    } catch (IllegalArgumentException arg) {
+      arg.printStackTrace();
+    } catch (IOException io){
+      io.printStackTrace();
+    }
+  }
+  public static void main(String args[]) throws Exception {
+    // Initialisation des variables
+    int portServeur = Integer.parseInt(args[0]);
+    int portClient = 20000;
+    int taillePaquet = Integer.parseInt(args[1]);
+    byte paquetByte[] = new byte[taillePaquet];
+    byte numPaquet = 0;
+    int indiceByte = 0;
+    FileInputStream file = new FileInputStream("message.txt");
+    DatagramSocket dsoc = new DatagramSocket(portClient);
+
+    paquetByte[0] = numPaquet++; // Ajout du numéro de paquet
+    for (indiceByte = 1; file.available() != 0; indiceByte++) {
+      if (indiceByte == taillePaquet){ // Dans le cas où le fichier n'est pas fini mais la taille de paquet est trop petite
+        sendPaquet(dsoc, paquetByte, taillePaquet, portServeur, portClient);
+        paquetByte = new byte[taillePaquet];
+        paquetByte[0] = numPaquet++;
+        indiceByte = 1;
+      }
+      paquetByte[indiceByte] = (byte) file.read();
+    }
+    file.close();
+
+    sendPaquet(dsoc, paquetByte, taillePaquet, portServeur, portClient);
+    dsoc.close();
+    System.out.println("Tout les paquets ont été émis et reçu");
+  }
 }
-
--- Reseaux/TP2/server.java
+++ Reseaux/TP2/server.java
@@ -1,22 +1,31 @@
 /*SERVER CLASS*/
-import java.net.*;
 import java.io.*;
-public class server
-{
-            public static void main(String args[])throws IOException
-            {
-                        byte b[]=new byte[66000];
-                        DatagramSocket dsoc=new DatagramSocket(Integer.parseInt(args[0]));
-                        FileOutputStream f=new FileOutputStream(args[1]);
-                        while(true)
-                        {
-                                    DatagramPacket dp=new DatagramPacket(b,b.length);
-                                    dsoc.receive(dp);
-                                    System.out.println(new String(b));
-                                    f.write(b);
-                                    System.out.println(dp.getLength());                             
+import java.net.*;
 
-                        }
-            }
+public class server {
+
+  public static void main(String args[]) throws IOException {
+    // Initialisation des variables
+    int portServeur = Integer.parseInt(args[0]);
+    int portClient = 20000;
+    int taillePaquet = 10;
+    byte b[] = new byte[taillePaquet];
+    DatagramSocket dsoc = new DatagramSocket(portServeur);
+    FileOutputStream f = new FileOutputStream(args[1]);
+
+    while (true) {
+      DatagramPacket dp = new DatagramPacket(b, b.length);
+      dsoc.receive(dp);
+
+      // Affichage des informations sur le paquet
+      System.out.println("Numéro du paquet : " + dp.getData()[0]);
+      System.out.println("Taille du paquet = "+ dp.getLength());
+      System.out.println("Contenu du paquet: " + new String(b));
+      f.write(dp.getData(), 1, dp.getLength() - 1); // Ecriture dans le fichier
+
+      // Renvoi de l'acquittement
+      String ack = "Paquet n°" + (dp.getData()[0] + " reçu.\n"); 
+      dsoc.send(new DatagramPacket(ack.getBytes(), ack.getBytes().length, InetAddress.getLocalHost(), portClient));
+    }
+  }
 }
-
