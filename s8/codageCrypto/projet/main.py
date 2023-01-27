b515354d86cf5d29d75ac2100ce3bae3dba02b0f
Daniel Pinson <daniel.pinson19@gmail.com>
2023-01-27 15:34:19 +0100

    Obtention clé encodage

--- s8/codageCrypto/projet/main.py
+++ s8/codageCrypto/projet/main.py
@@ -1,25 +1,74 @@
 import random as rdn
 import numpy as np
+from jokerNoir import reculJokerNoir
+from jokerRouge import reculJokerRouge
 
 # Les jokers sont 53 et 54, noir étant 53
 
 
-# Prend le paquet en argument et le renvoi
-def reculJokerNoir(p):
-    i = np.where(p == 53)[0][0]
-    if (i == 53):
-        p[[i, 1]] = p[[1, i]]
-    else:
-        p[[i, i+1]] = p[[i+1, i]]
+def coupeDouble(p):
+    iNoir = np.where(p == 53)[0][0]
+    iRouge = np.where(p == 54)[0][0]
+    p1, p2, p3 = p[:min(iNoir, iRouge)], p[min(iNoir, iRouge):max(
+        iNoir, iRouge)+1], p[max(iNoir, iRouge)+1:]
+    return np.concatenate((p3, p2, p1))
+
+
+def coupeSimple(p):
+    carte = p[53] if p[53] != 54 else 53
+    cartesABouger, reste = p[:carte], p[carte:53]
+    return np.concatenate((reste, cartesABouger, [carte]))
+
+
+def melange(p):
+    p = reculJokerNoir(p)
+    p = reculJokerRouge(p)
+    p = coupeDouble(p)
+    p = coupeSimple(p)
     return p
 
 
+def lecturePseudoAleatoire(p):
+    n = p[0] if p[0] != 54 else 53
+    m = p[n]
+    if (m >= 53):
+        p = melange(p)
+        lecturePseudoAleatoire(p)
+    elif (m > 26):
+        m = m - 26
+    return m
+
+
+def testMano(paquet):
+    print(f"{paquet=}")
+    paquetNoir = reculJokerNoir(paquet)
+    print(f"{paquetNoir=}")
+    paquetRouge = reculJokerRouge(paquet)
+    print(f"{paquetRouge=}")
+    paquetCoupe = coupeDouble(paquet)
+    print(f"{paquetCoupe=}")
+    paquetCoupeSimple = coupeSimple(paquet)
+    print(f"{paquetCoupeSimple=}")
+    # lettre = lecturePseudoAleatoire(paquet)
+    # print(f"{alphabet[lettre]=}")
+
+
+# Prend le paquet en argument et le renvoi
 def main():
     rdn.seed(1)
+
+    alphabet = dict()
+    for i in range(0, 26):
+        alphabet[i+1] = chr(65 + i)
     paquet = np.array(rdn.sample(range(1, 55), 54))
-    print(f"{paquet=}")
-    paquet = reculJokerNoir(paquet)
-    print(f"{paquet=}")
+
+    tabCleEncodage = []
+    for i in range(26):
+        paquet = melange(paquet)
+        tabCleEncodage.append(alphabet[lecturePseudoAleatoire(paquet)])
+    cleEncodage = ''.join(tabCleEncodage)
+    print(f"{cleEncodage=}")
+
 
 
 if __name__ == "__main__":
--- s8/codageCrypto/projet/test.py
+++ s8/codageCrypto/projet/test.py
@@ -2,6 +2,15 @@ import numpy as np
 from main import reculJokerNoir
 
 
+def testReculJoker():
+    print("Test Joker avec un paquet normal")
+    paquet = np.array([x for x in range(0, 55)])
+    paquet = reculJokerNoir(paquet)
+    verif = np.array([x for x in range(0, 55)])
+    verif[[1, 53]] = verif[[53, 1]]
+    np.array_equal(paquet, verif)
+
+
 def testJokerDernierePosition():
     print("Test Joker en dernière position")
     paquet = np.array([x for x in range(0, 55)])
