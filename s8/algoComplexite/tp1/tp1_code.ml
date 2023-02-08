59db1405a050c025a3c12d0ab1d379d136195206
= <=>
2023-02-08 10:02:22 +0100

    insertion fichier caml qui marche

--- s8/algoComplexite/tp1/tp1_code.ml
+++ s8/algoComplexite/tp1/tp1_code.ml
@@ -5,45 +5,57 @@
 (* Pour lancer une session interactive d'OCaml dans un terminal : rlwrap ocaml. *)
 
 (* Pour exécuter du code OCaml situe dans un fichier .ml depuis la 
-   session interactive : #use "nom_du_fichier.ml" ;; 
+session interactive : #use "nom_du_fichier.ml" ;; 
 
-   PS: Bien sur il faut changer nom_du_fichier par le vrai nom de votre fichier ... :) *)
+PS: Bien sur il faut changer nom_du_fichier par le vrai nom de votre fichier ... :) *)
 
 (* Pour quitter la session interactive : #quit ;; *)
 
 (* NB: Pour pouvoir trier des grandes listes, pensez a executer "export OCAMLRUNPARAM='l=4M'" dans 
-   votre terminal avant de faire appel a rlwrap ocaml. *)
+votre terminal avant de faire appel a rlwrap ocaml. *)
 
 
 
 (* ----- Tri par insertion sans fonction de comparaison. *)
 
 let rec insere_dans_liste_triee element liste =
-    match liste with 
-      | [] -> [element]
-      | elt1::reste -> 
-        if element < elt1 then element::liste 
-        else elt1::(insere_dans_liste_triee element reste) ;;
+	match liste with 
+	| [] -> [element]
+	| elt1::reste -> 
+		if element < elt1 then element::liste 
+		else elt1::(insere_dans_liste_triee element reste) ;;
 
 let rec tri_insertion liste =
-    match liste with
-      | [] -> []
-      | elt1::reste -> insere_dans_liste_triee elt1 (tri_insertion reste) ;;
+	match liste with
+	| [] -> []
+	| elt1::reste -> insere_dans_liste_triee elt1 (tri_insertion reste) ;;
 
 (* ----- Tri par insertion avec fonction de comparaison. *)
 
 let rec insere_dans_liste_triee element liste fct_comparaison =
-    match liste with 
-      | [] -> [element]
-      | elt1::reste -> 
-        if fct_comparaison element elt1 
-        then element::liste 
-        else elt1::(insere_dans_liste_triee element reste fct_comparaison) ;;
+	match liste with 
+	| [] -> [element]
+	| elt1::reste -> 
+		if fct_comparaison element elt1 
+		then element::liste 
+		else elt1::(insere_dans_liste_triee element reste fct_comparaison) ;;
 
 let rec tri_insertion liste fct_comparaison =
-    match liste with
-      | [] -> []
-      | elt1::reste -> insere_dans_liste_triee elt1 (tri_insertion reste fct_comparaison) fct_comparaison ;;
+	match liste with
+	| [] -> []
+	| elt1::reste -> insere_dans_liste_triee elt1 (tri_insertion reste fct_comparaison) fct_comparaison ;;
+
+(*Fonction de comparaison*)
+let cmp_croissant ent1 ent2 =
+        ent1 < ent2 ;;
+
+let cmp_decroissant ent1 ent2 =
+        ent1 > ent2 ;;
+
+let cmp_coo_croissant coord1 coord2 =
+        match coord1 with
+        (x1, y1) -> match coord2 with
+                    | (x2, y2) -> x1 < x2 ;;
 
 (* ----- Tests du tri par insertion. *)
 
@@ -60,28 +72,41 @@ tri_insertion liste_coo_a_trier cmp_coo_croissant ;;
 (* A completer bien sur sinon ca marche beaucoup moins bien. *)
 
 let rec coupe_en_2 liste = 
-    match ___ with 
-      | [] -> ___, ___
-      | [_] -> ___, ___
-      | elt1::elt2::reste -> 
-        let partie1, partie2 = ___ in 
-        elt1::partie1, elt2::partie2 ;;
-
+  match liste with 
+    | [] -> [], []
+    | [_] -> liste, []
+    | elt1::elt2::reste -> 
+        let partie1, partie2 = coupe_en_2 reste in 
+            elt1::partie1, elt2::partie2 ;;
+        
 (* ----- Fonction fusionne du tri fusion. *)
 (* Meme remarque que precedemment. *)
 
 let rec fusionne liste1 liste2 fct_comparaison = 
-    match liste1, liste2 with 
-      | [], _ -> ___
-      | _, [] -> ___
-      | ___, ___ -> 
-        if ___
+  match liste1, liste2 with 
+    | [], liste2 -> liste2
+    | liste1, [] -> liste1
+    | tete1::queue1, tete2::queue2 -> 
+        if fct_comparaison tete1 tete2
         then tete1::(fusionne queue1 liste2 fct_comparaison) 
         else tete2::(fusionne liste1 queue2 fct_comparaison) ;;
 
 
+let rec tri_fusion liste fct_comparaison =
+  match liste with
+    | [] -> []
+    | [_] -> liste
+    | elt1::elt2::reste ->
+        let a, b = coupe_en_2 liste in
+          fusionne (tri_fusion a fct_comparaison) (tri_fusion b fct_comparaison) fct_comparaison;;
+
+
+tri_fusion liste_a_trier cmp_croissant ;;
+tri_fusion liste_a_trier cmp_decroissant ;;
+tri_fusion liste_coo_a_trier cmp_coo_croissant ;;
 
 (* ----- Definition du type arbre et fonction principale du tri par arbre. *)
+(*
 
 type 't arbre = Vide | Noeud of 't arbre * 't * 't arbre ;;
 
@@ -94,16 +119,17 @@ let tri_arbre liste fct_comparaison =
 
 let fonction_qui_utilise_un arbre = 
     match arbre with
-      | Vide -> (* Les instructions si l'arbre est un noeud vide. *)
-      | Noeud(fils_gauche, valeur, fils_droit) -> (* Les instructions si l'arbre n'est pas un noeud vide. *)
+    | Vide -> (* Les instructions si l'arbre est un noeud vide. *)
+    | Noeud(fils_gauche, valeur, fils_droit) -> (* Les instructions si l'arbre n'est pas un noeud vide. *)
 
-        (* ----- Exemples d'instances d'arbres. *)
+(* ----- Exemples d'instances d'arbres. *)
 
-        (* Arbre vide. *)
-        let arbre_vide = Vide ;;
+(* Arbre vide. *)
+let arbre_vide = Vide ;;
 (* Arbre d'entiers compose d'un seul element (= feuille). *)
 let arbre_feuille = Noeud(Vide, 1, Vide) ;;
 (* Arbre d'entiers compose d'un seul noeud avec un fils. *)
 let arbre_un_fils = Noeud(Vide, 1, Noeud(Vide, 2, Vide)) ;;
 (* Arbre d'entiers compose d'un seul noeud avec deux fils. *)
 let arbre_deux_fils = Noeud(Noeud(Vide, 0, Vide), 1, Noeud(Vide, 2, Vide)) ;;
+*)
