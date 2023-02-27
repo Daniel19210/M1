0255feb2f35d9eeccff2c5cb59ac52a21f654341
Daniel Pinson <daniel.pinson19@gmail.com>
2023-02-27 10:15:03 +0100

    fix merge conflict

--- s8/algoComplexite/tp1/tp1_code.ml
+++ s8/algoComplexite/tp1/tp1_code.ml
@@ -26,10 +26,10 @@ let rec insere_dans_liste_triee element liste =
 		if element < elt1 then element::liste 
 		else elt1::(insere_dans_liste_triee element reste) ;;
 
-let rec tri_insertion liste =
+let rec tri_insertion_brut liste =
 	match liste with
 	| [] -> []
-	| elt1::reste -> insere_dans_liste_triee elt1 (tri_insertion reste) ;;
+	| elt1::reste -> insere_dans_liste_triee elt1 (tri_insertion_brut reste) ;;
 
 (* ----- Tri par insertion avec fonction de comparaison. *)
 
@@ -55,13 +55,15 @@ let cmp_decroissant ent1 ent2 =
 
 let cmp_coo_croissant coord1 coord2 =
         match coord1 with
-        (x1, y1) -> match coord2 with
-                    | (x2, y2) -> x1 < x2 ;;
+        (x1, _) -> match coord2 with
+                    | (x2, _) -> x1 < x2 ;;
 
 (* ----- Tests du tri par insertion. *)
 
 let liste_a_trier = [9;0;5;9;6] ;;
 let liste_coo_a_trier = [(9,1);(0,2);(5,3);(9,4);(6,5)] ;;
+
+tri_insertion_brut liste_a_trier ;;
 
 tri_insertion liste_a_trier cmp_croissant ;;
 tri_insertion liste_a_trier cmp_decroissant ;;
@@ -71,7 +73,6 @@ tri_insertion liste_coo_a_trier cmp_coo_croissant ;;
 
 (* ----- Fonction coupe_en_2 du tri fusion. *)
 (* A completer bien sur sinon ca marche beaucoup moins bien. *)
-(*
 let rec coupe_en_2 liste = 
   match liste with 
     | [] -> [], []
@@ -79,7 +80,7 @@ let rec coupe_en_2 liste =
     | elt1::elt2::reste -> 
         let partie1, partie2 = coupe_en_2 reste in 
             elt1::partie1, elt2::partie2 ;;
-        
+
 (* ----- Fonction fusionne du tri fusion. *)
 (* Meme remarque que precedemment. *)
 
@@ -97,7 +98,7 @@ let rec tri_fusion liste fct_comparaison =
   match liste with
     | [] -> []
     | [_] -> liste
-    | elt1::elt2::reste ->
+    | _::_::_ ->
         let a, b = coupe_en_2 liste in
           fusionne (tri_fusion a fct_comparaison) (tri_fusion b fct_comparaison) fct_comparaison;;
 
@@ -109,7 +110,8 @@ print_string "Liste non triée : ";;
 let () = List.iter (printf "%d ") liste_a_trier;;
 print_string "\nListe triée avec un tri fusion : ";;
 let () = List.iter (printf "%d ") liste_triee_fusion;;
-*)
+
+printf("\n");;
 (* ----- Tri quicksort *)
 
 (* ----- Definition du type arbre et fonction principale du tri par arbre. *)
