7b347a0c5d425fa6d7905686e562fbb037ae2823
Daniel Pinson <daniel.pinson19@gmail.com>
2023-03-03 13:36:37 +0100

    traitement d'image tp4 Début application de plus de filtre

--- s8/traitementImage/tp4/main.m
+++ s8/traitementImage/tp4/main.m
@@ -1,8 +1,13 @@
 I = imread("../TP/NG/lena256.png");
-test = reshape([2,2,2,2,0,12,1,51,0, 60, 0, 50], 4, 3);
-normeI = normeGradient(I)
-imshow(uint8(normeI));
-%imshow(imgradient(I)/255)
+%test = reshape([2,2,2,2,0,12,1,51,0, 60, 0, 50], 4, 3)
+normeISimple = normeGradient(I, 'Simple');
+%normeI = normeGradient(I, 'Roberts');
+subplot(1,3,1)
+imshow(I);
+subplot(1,3,2)
+imshow(uint8(normeISimple));
+subplot(1,3,3)
+imshow(imgradient(I)/255)
 waitfor(gcf);
 %abs(normeI - imgradient(I))
 %imshow(laplacien(I, 4))
--- s8/traitementImage/tp4/normeGradient.m
+++ s8/traitementImage/tp4/normeGradient.m
@@ -1,22 +1,27 @@
-function norme = normeGradient(Image)
+function norme = normeGradient(Image, type)
     [n,m,can]=size(Image);
-    fx = [-1, 1];
-    fy = reshape([-1, 1], 2, 1);
     Gx = zeros(n,m);
     Gy = zeros(n,m);
-    for ligne = 1:n
-        for colonne = 1:m
-            if (colonne < m)
-                Gx(ligne, colonne) = Image(ligne, colonne) * fx(1) + Image(ligne, colonne+1) * fx(2);
-            else
-                Gx(ligne, colonne) = Image(ligne, colonne) * fx(1);
-            end
-            if (ligne < n)
-                Gy(ligne, colonne) = Image(ligne, colonne) * fy(1, 1) + Image(ligne+1, colonne) * fy(2, 1);
-            else
-                Gy(ligne, colonne) = Image(ligne, colonne) * fy(1, 1);
+    if (strcmp(type, 'Simple'))
+        fx = [-1, 1];
+        fy = reshape([-1, 1], 2, 1);
+        for ligne = 1:n
+            for colonne = 1:m
+                if (colonne < m)
+                    Gx(ligne, colonne) = Image(ligne, colonne) * fx(1) + Image(ligne, colonne+1) * fx(2);
+                else
+                    Gx(ligne, colonne) = Image(ligne, colonne) * fx(1);
+                end
+                if (ligne < n)
+                    Gy(ligne, colonne) = Image(ligne, colonne) * fy(1, 1) + Image(ligne+1, colonne) * fy(2, 1);
+                else
+                    Gy(ligne, colonne) = Image(ligne, colonne) * fy(1, 1);
+                end
             end
         end
+        norme = abs(Gx) + abs(Gy);
+    else if (strcmp(type, 'Roberts'))
+        fx = reshape([1, 0, 0, -1], 2, 2)
+        fy = reshape([0, -1, 1, 0], 2, 2)
     end
-    norme = abs(Gx) + abs(Gy)
 end
