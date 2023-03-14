3083af63b84cbd7edef6597760d8b9662e93750c
= <=>
2023-03-14 14:47:29 +0100

    Codage&Crypto Ajout d'un test unitaire

--- s8/codageCrypto/projet/test.py
+++ s8/codageCrypto/projet/test.py
@@ -1,6 +1,6 @@
 import numpy as np
 import inspect
-from chiffrement import reculJokerNoir, reculJokerRouge
+from chiffrement import reculJokerNoir, reculJokerRouge, coupeDouble
 
 
 def testReculJokerNoirPaquetNeuf():
@@ -14,14 +14,16 @@ def testJokerNoirDernierePosition():
     paquet = np.array([x for x in range(1, 53)] + [54])
     paquet = np.insert(paquet, 53, 53)  # Insertion en dernière position
     paquet = reculJokerNoir(paquet)
-    temoin = np.array([53]+[x for x in range(2, 53)]+[54, 1])
+    temoin = np.array([x for x in range(1, 53)] + [54])
+    temoin = np.insert(temoin, 1, 53)
     print("Passed :", inspect.getframeinfo(inspect.currentframe()).function) if np.array_equal(paquet, temoin) else print( "Failed :", inspect.getframeinfo(inspect.currentframe()).function, f"\n\t{paquet=}\n\t{temoin=}")
 
 
 def testReculJokerRougeDernierePosition():
     paquet = np.array([x for x in range(1, 55)])
     paquet = reculJokerRouge(paquet)
-    temoin = np.array([2, 54]+[x for x in range(3, 54)]+[1])
+    temoin = np.array([x for x in range(1,54)])
+    temoin = np.insert(temoin, 2, 54)
     print("Passed :", inspect.getframeinfo(inspect.currentframe()).function) if np.array_equal(paquet, temoin) else print( "Failed :", inspect.getframeinfo(inspect.currentframe()).function, f"\n\t{paquet=}\n\t{temoin=}")
 
 
@@ -29,7 +31,8 @@ def testReculJokerRougeAvantDernierePosition():
     paquet = np.array([x for x in range(1, 54)])
     paquet = np.insert(paquet, 52, 54)
     paquet = reculJokerRouge(paquet)
-    temoin = np.array([54]+[x for x in range(2, 54)]+[1])
+    temoin = np.array([x for x in range(1,54)])
+    temoin = np.insert(temoin, 1, 54)
     print("Passed :", inspect.getframeinfo(inspect.currentframe()).function) if np.array_equal(paquet, temoin) else print( "Failed :", inspect.getframeinfo(inspect.currentframe()).function, f"\n\t{paquet=}\n\t{temoin=}")
 
 
@@ -40,6 +43,17 @@ def testReculJokerRougePositionNormale():
     temoin = np.array([x for x in range(1, 28)]+[54]+[x for x in range(28, 54)])
     print("Passed :", inspect.getframeinfo(inspect.currentframe()).function) if np.array_equal(paquet, temoin) else print( "Failed :", inspect.getframeinfo(inspect.currentframe()).function, f"\n\t{paquet=}\n\t{temoin=}")
 
+def testCoupeDouble():
+    paquet = np.array([x for x in range(1,53)])
+    paquet = np.insert(paquet, 7, 54)
+    paquet = np.insert(paquet, 42, 53)
+    paquet = coupeDouble(paquet)
+
+    p1 = np.array([x for x in range(42, 53)])
+    p2 = np.concatenate(([54], np.array([x for x in range(8, 42)]), [53]))
+    p3 = np.array([x for x in range(1,8)])
+    temoin = np.concatenate((p1, p2, p3))
+    print("Passed :", inspect.getframeinfo(inspect.currentframe()).function) if np.array_equal(paquet, temoin) else print( "Failed :", inspect.getframeinfo(inspect.currentframe()).function, f"\n\t{paquet=}\n\t{temoin=}")
 
 def testSuite():
     testReculJokerNoirPaquetNeuf()
@@ -47,6 +61,7 @@ def testSuite():
     testReculJokerRougeDernierePosition()
     testReculJokerRougeAvantDernierePosition()
     testReculJokerRougePositionNormale()
+    testCoupeDouble()
     print("Testsuite terminée")
 
 
