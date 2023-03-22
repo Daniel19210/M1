3bc48b9a186af6a23bedac17ce936d825df8b1af
Daniel Pinson <daniel.pinson19@gmail.com>
2023-03-22 14:35:05 +0100

    Algorithmique et Complexitée Ajout du TP4, tableau dynamique pas terminé

--- s8/algoComplexite/tp3/tp3_code.ml
+++ s8/algoComplexite/tp3/tp3_code.ml
@@ -12,13 +12,80 @@ PS: Bien sur il faut changer nom_du_fichier par le vrai nom de votre fichier ...
 (* Pour quitter la session interactive : #quit ;; *)
 
 (* ----- Definition du type graphe. *)
+open Printf;;
 
 type 'a graphe = { 
     sommets: 'a array; 
-    aretes: ((int * float) list) array 
+    aretes: ((int * float) list) array (*Liste d'adjascence*)
 } ;;
 
+let grapheComplet = {
+                        sommets = [|"A"; "B"; "C"; "D"; "E"; "F"; "G"; "H"; "I"; "J"|];
+                        aretes = [|
+                            [(1, 85.); (2, 217.); (4, 173.)];    (* 0, A *)
+                            [(0, 85.); (5, 80.)];                (* 1, B *)
+                            [(0, 217.); (6, 186.); (7, 103.)];   (* 2, C *)
+                            [(7, 183.)];                         (* 3, D *)
+                            [(0, 173.); (9, 502.)];              (* 4, E *)
+                            [(1, 80.); (8, 250.)];               (* 5, F *)
+                            [(2, 186.)];                         (* 6, G *)
+                            [(2, 103.); (3, 183.); (9, 167.)];   (* 7, H *)
+                            [(5, 250.); (9, 84.)];               (* 8, I *)
+                            [(4, 502.); (7, 167.); (8, 84.)];    (* 9, J *)
+                            |]
+                    };;
 
+let rec get_indice_min_array_rec array sommet_marque elemMin indiceMin nbRec =
+    if Array.length array == 0 then
+        indiceMin
+    else
+        if ( array.(0) >= elemMin || Array.exists (fun a -> a == nbRec) sommet_marque ) then
+            get_indice_min_array_rec (Array.sub array 1 ((Array.length array)-1)) sommet_marque elemMin indiceMin (nbRec+1)
+        else
+            get_indice_min_array_rec (Array.sub array 1 ((Array.length array)-1)) sommet_marque array.(0) nbRec (nbRec+1);;
+
+let rec get_indice_min_array_init array sommet_marque =
+    get_indice_min_array_rec array sommet_marque infinity (-1) 0 ;;
+
+let rec traitement_liste_voisin listeVoisin indice_sommet_min tab_d tab_p =
+    match listeVoisin with
+    | [] -> tab_d, tab_p
+    | tuple_voisin::voisin_restant -> let dist_voisin =
+            if (tab_d.(indice_sommet_min) == infinity) then
+                snd tuple_voisin
+            else
+                tab_d.(indice_sommet_min) +. snd tuple_voisin
+        in
+        if dist_voisin < tab_d.(fst tuple_voisin) then (
+            tab_d.(fst tuple_voisin) <- dist_voisin ;
+            tab_p.(fst tuple_voisin) <- indice_sommet_min ;
+        );
+        traitement_liste_voisin voisin_restant indice_sommet_min tab_d tab_p ;;
+
+let rec traitement_point graphe tab_d tab_p tab_m =
+    if (Array.length tab_m == Array.length tab_d)  then
+        (tab_d, tab_p)
+    else (
+        let indice_sommet_min = get_indice_min_array_init tab_d tab_m in
+        let liste_voisin_min = graphe.aretes.(indice_sommet_min) in
+        let tab_d, tab_p = traitement_liste_voisin liste_voisin_min indice_sommet_min tab_d tab_p in
+        let tab_m = Array.append tab_m [| indice_sommet_min |] in
+        traitement_point graphe tab_d tab_p tab_m;
+    );;
+
+let dijkstra graphe indice_depart =
+    let n = Array.length graphe.sommets in
+    let tab_d = Array.make n infinity in
+    let tab_p = [| -1; -1; -1; -1; -1; -1; -1; -1; -1; -1 |] in
+    let tab_m = [| |] in
+    tab_d.(indice_depart) <- 0. ;
+    tab_p.(indice_depart) <- indice_depart ;
+    traitement_point graphe tab_d tab_p tab_m;;
+
+let arrayD, arrayP = dijkstra grapheComplet 0 ;;
+print_string("\nDijsktra avec liste\n");;
+print_string("tableau des distance = [ ") ;; Array.iter (printf "%.0f. ") arrayD;; print_string("]\n");;
+print_string("tableau des prédécesseurs = [ ") ;; Array.iter (printf "%d ") arrayP;; print_string("]\n");;
 
 (* ----- Definition du type tas. *)
 
@@ -54,13 +121,99 @@ let vider tas =
 
 let creer_tas liste_elements = ajouter_plusieurs liste_elements Vide ;;
 
+let sequence from to_ =
+    let rec sequence_rt from to_ acc =
+        if from >= to_ then acc
+        else sequence_rt (from + 1) to_ (acc @ [from])
+    in sequence_rt from to_ [] ;;
+
+let dijkstra_tas graphe sommet_depart =
+    let nb_sommets = Array.length graphe.sommets in
+    let distances = Array.make nb_sommets infinity in
+    let predecesseurs = Array.make nb_sommets (-1) in
+
+    distances.(sommet_depart) <- 0. ; predecesseurs.(sommet_depart) <- sommet_depart ;
+
+    let rec dijkstra_aux tas_sommets_restants =
+        match tas_sommets_restants with
+        | Vide -> distances, predecesseurs
+        | Noeud (fils_gauche, sommet_dist_min, fils_droit) -> (
+            List.iter (function (destination, poids) -> 
+                let nouvelle_dist = distances.(sommet_dist_min) +. poids in
+                if nouvelle_dist < distances.(destination) 
+                then (distances.(destination) <- nouvelle_dist; predecesseurs.(destination) <- sommet_dist_min)
+            ) graphe.aretes.(sommet_dist_min) ;
+            dijkstra_aux (supprimer_premier_noeud tas_sommets_restants)
+        )
+    in dijkstra_aux (creer_tas (sequence 0 nb_sommets)) ;;
+
+creer_tas (sequence 0 10);;
+
+let arrayD, arrayP = dijkstra_tas grapheComplet 0 ;;
+print_string("\nDijkstra solution du professeur avec tas manuel\n");;
+print_string("tableau des distance = [ ") ;; Array.iter (printf "%.0f. ") arrayD;; print_string("]\n");;
+print_string("tableau des prédécesseurs = [ ") ;; Array.iter (printf "%d ") arrayP;; print_string("]\n");;
+
+let dijkstra_prof graphe sommet_depart = 
+    let nb_sommets = Array.length graphe.sommets in
+    let distances = Array.make nb_sommets infinity in
+    let predecesseurs = Array.make nb_sommets (-1) in
+
+    distances.(sommet_depart) <- 0. ; predecesseurs.(sommet_depart) <- sommet_depart ;
+
+    let rec dijkstra_aux sommets_restants =
+        match sommets_restants with
+        | [] -> distances, predecesseurs
+        | elt1::reste -> ( 
+            let sommet_dist_min = List.fold_left (fun i j -> if distances.(i) < distances.(j) then i else j) elt1 reste in
+            List.iter (function (destination, poids) -> 
+                let nouvelle_dist = distances.(sommet_dist_min) +. poids in
+                if nouvelle_dist < distances.(destination) 
+                then (distances.(destination) <- nouvelle_dist; predecesseurs.(destination) <- sommet_dist_min)
+            ) graphe.aretes.(sommet_dist_min) ;
+            dijkstra_aux (List.filter (function sommet -> sommet <> sommet_dist_min) sommets_restants)
+        ) 
+    in dijkstra_aux (sequence 0 nb_sommets) ;;
+
+let arrayD, arrayP = dijkstra_prof grapheComplet 0 ;;
+print_string("\nDijkstra solution du professeur\n");;
+print_string("tableau des distance = [ ") ;; Array.iter (printf "%.0f. ") arrayD;; print_string("]\n");;
+print_string("tableau des prédécesseurs = [ ") ;; Array.iter (printf "%d ") arrayP;; print_string("]\n");;
+
+let dijkstra graphe sommet_depart =
+    let nb_sommets = Array.length graphe.sommets in
+    let distances = Array.make nb_sommets infinity in 
+    let predecesseurs = Array.make nb_sommets (-1) in 
+
+    distances.(sommet_depart) <- 0. ; predecesseurs.(sommet_depart) <- sommet_depart ;
+
+    let rec dijkstra_aux tas =
+        match tas with
+        | Vide -> distances, predecesseurs
+        | Noeud(_, (sommet_dist_min, dist_depuis_depart), _) ->
+            let nouveau_tas = supprimer_premier_noeud tas in
+            if dist_depuis_depart > distances.(sommet_dist_min)
+            then dijkstra_aux nouveau_tas
+            else dijkstra_aux (List.fold_left (fun noeud (sommet, dist_actuelle) ->
+                let nouvelle_dist = distances.(sommet_dist_min) +. dist_actuelle in
+                if nouvelle_dist < distances.(sommet)
+                then (distances.(sommet) <- nouvelle_dist ; predecesseurs.(sommet) <- sommet_dist_min ; ajouter (sommet, distances.(sommet)) noeud)
+                else noeud) nouveau_tas graphe.aretes.(sommet_dist_min)
+            )
+	in
+    dijkstra_aux (creer_tas [(sommet_depart, 0.)]) ;;
+
+let arrayD, arrayP = dijkstra_prof grapheComplet 0 ;;
+print_string("\nDijkstra solution du professeur avec tas\n");;
+print_string("tableau des distance = [ ") ;; Array.iter (printf "%.0f. ") arrayD;; print_string("]\n");;
+print_string("tableau des prédécesseurs = [ ") ;; Array.iter (printf "%d ") arrayP;; print_string("]\n");;
 
 
 (* ----- Chargement du module pour faire du graphique. *)
 
-#load "graphics.cma";;
-open Graphics ;;
+(*open Graphics ;;*)
 
+(*
 (* ----- Genere un ensemble de sommets sur l'interface graphique en evitant les coordonnees situees dans les trous. *)
 
 let genere_sommets nb_sommets trous =
@@ -148,3 +301,4 @@ let affichage_chemins sommets aretes sommet_depart =
     dessine_chemin sommets predecesseurs sommet_depart 0 ;
     dessine_chemin sommets predecesseurs sommet_depart ((Array.length aretes) - 1) ;
     set_color black ;;
+ *)
