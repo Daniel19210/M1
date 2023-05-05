1a867dda7be1bf3d7751ee646176915ffb605e8c
Daniel Pinson <daniel.pinson19@gmail.com>
2023-05-05 11:52:31 +0200

    Ajout de requêtes

--- s8/sysGestionDocument/projet/recupInfo.py
+++ s8/sysGestionDocument/projet/recupInfo.py
@@ -21,21 +21,64 @@ def getAllNameFilm():
 
 
 def get_films_from_cinema(nom_cinoch):
-    res = []
-    films_du_cinema = list(db.cinemas.find({"nom": nom_cinoch},
-                                           {"film_et_Entrees": 1, "_id": 0}
-                                           ))[0]["film_et_Entrees"]
-    for idFilm in films_du_cinema:
-        res.append(db.films.find({"id": idFilm["idFilm"]}))
-    return res
+    return db.cinemas.aggregate(
+        [
+            {
+                '$match': {
+                    'nom': 'Olympia'
+                }
+            }, {
+                '$project': {
+                    'film_et_Entrees': 1
+                }
+            }, {
+                '$lookup': {
+                    'from': 'films',
+                    'localField': 'film_et_Entrees.idFilm',
+                    'foreignField': 'id',
+                    'as': 'film_olympia'
+                }
+            }, {
+                '$project': {
+                    'film_olympia': 1
+                }
+            }, {
+                '$unwind': {
+                    'path': '$film_olympia'
+                }
+            }
+        ]
+    )
+    # res = []
+    # films_du_cinema = list(db.cinemas.find({"nom": nom_cinoch},
+    # {"film_et_Entrees": 1, "_id": 0}
+    # ))[0]["film_et_Entrees"]
+    # for idFilm in films_du_cinema:
+    # res.append(db.films.find({"id": idFilm["idFilm"]}))
+    # return res
 
 
 def get_commentaires_film(nom_film):
     return db.films.find({"Titre": nom_film},
                          {"_id": 0, "Note_et_Commentaires.commentaire": 1})
 
+"""
+var map = function(){emit(this.nom, this.id);}
+var reduce = function(nomCinema, nbEntrees){
+    return Array.sum(nbEntrees);
+}
+db.cinemas.mapReduce(map, reduce, {out: {inline: 1}})
+"""
 
-print(f'{getAllNameFilm() =}')
-pprint(list(getAllNameCinema()))
-pprint(list(get_films_from_cinema("Olympia")[0]))
-pprint(list(get_commentaires_film("Le seigneur des anneaux")[0]))
+# print(f'{getAllNameFilm() =}')
+# pprint(list(getAllNameCinema()))
+pprint(list(get_films_from_cinema("Olympia")))
+# pprint(list(get_commentaires_film("Le seigneur des anneaux")[0]))
+
+# En mongoDB
+# tous les commentaires avec la note d'un film
+# Le nombre d'entrée total d'un cinéma pour chaque cinema avec un map reduce
+# La moyenne des entrées d'un cinéma pour chaque cinema avec un map reduce
+
+# En Pymongo
+# La note moyenne d'un film (demander quel film)
